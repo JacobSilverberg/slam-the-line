ce0eae42df4a05762f7180564abcf1bc83b0301b
Jacob Silverberg <minkus962@gmail.com>
2024-08-09 18:01:12 -0400

    test env vars

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
+
 const Home = () => {
   return (
     <div className="home">
       <h1>Want to kick your friend&apos;s asses?</h1>
       <h2>Season-long ATS and Pick &apos;em fantasy leagues.</h2>
+      <h3>${apiUrl}</h3>
       <span>
         Want to join the fun? <Link to="/register">Register now!</Link>
       </span>
