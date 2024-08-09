bf7a73e763f708ea7aa5b6c5b9124bd58d5c61a4
Jacob Silverberg <minkus962@gmail.com>
2024-08-08 21:31:34 -0400

    update auth

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,25 +1,53 @@
 import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import {jwtDecode} from 'jwt-decode'; // Import jwt_decode correctly
 
 const AuthContext = createContext();
 
+const validateToken = (token) => {
+  if (!token || typeof token !== 'string') {
+    // No token or invalid token type, return null without logging
+    return null;
+  }
+
+  try {
+    const decodedToken = jwtDecode(token);
+    if (decodedToken.exp * 1000 < Date.now()) {
+      // Token has expired
+      return null;
+    }
+    return token;
+  } catch (error) {
+    console.error('Invalid token:', error);
+    return null;
+  }
+};
+
 const AuthProvider = ({ children }) => {
-  const [authToken, setAuthToken] = useState(
-    localStorage.getItem('token') || null
-  );
+  const [authToken, setAuthToken] = useState(() => {
+    const token = localStorage.getItem('token');
+    return validateToken(token);
+  });
 
   const isAuthenticated = Boolean(authToken);
 
   useEffect(() => {
-    localStorage.setItem('token', authToken);
+    if (authToken) {
+      localStorage.setItem('token', authToken);
+    } else {
+      localStorage.removeItem('token');
+    }
   }, [authToken]);
 
   const login = (token) => {
-    setAuthToken(token);
+    const validatedToken = validateToken(token);
+    if (validatedToken) {
+      setAuthToken(validatedToken);
+    }
   };
 
   const logout = () => {
-    // setAuthToken(null);
+    setAuthToken(null);
     localStorage.removeItem('token');
     console.log('Logged out');
   };
