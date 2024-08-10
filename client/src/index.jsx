4bb96b0ce2542f288b371b7494cf511ad3bbac62
Jacob Silverberg <minkus962@gmail.com>
2024-08-10 16:46:43 -0400

    index test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { WeekProvider } from './context/WeekContext.jsx';
 
+console.log("VITEAPIURL @ INDEX.JSX: ", process.env.VITE_API_URL)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <WeekProvider>
