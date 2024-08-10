0bd4b29ef906836270a1eb086c616c8de8ce9b42
Jacob Silverberg <minkus962@gmail.com>
2024-08-10 19:34:49 -0400

    testing mode

--- client/src/services/serverConfig.js
+++ client/src/services/serverConfig.js
@@ -1,5 +1,15 @@
-const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+let apiUrl;
 
-console.log('apiUrl at serverConfig', apiUrl);
+console.log('mode', import.meta.env.MODE);
+
+if (import.meta.env.MODE === 'development') {
+  // When running in development mode (npm run dev)
+  apiUrl = 'http://localhost:3000';
+} else {
+  // When running in production or preview mode (vite preview)
+  apiUrl = import.meta.env.VITE_API_URL;
+}
+
+console.log('apiUrl at serverConfig:', apiUrl);
 
 export default apiUrl;
