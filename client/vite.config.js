43d4e5bebe40f1e25ecb7192324d12fb3c313494
Jacob Silverberg <minkus962@gmail.com>
2024-08-10 16:50:51 -0400

    update import

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-console.log('VITE_API_URL:', process.env.VITE_API_URL);
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Bind to all network interfaces
-    port: process.env.PORT || 5173, // Use the Railway-provided port or default to 3000
+    port: import.meta.env.PORT || 5173, // Use the Railway-provided port or default to 3000
   },
   preview: {
     host: '0.0.0.0', // Ensure preview also binds to all interfaces
-    port: process.env.PORT || 5173, // Use the Railway-provided port or default to 3000
+    port: import.meta.env.PORT || 5173, // Use the Railway-provided port or default to 3000
   },
 });
